20a6e6608f886059ae4655253ee2c922e63ec2ae
Victor Yee <victyee@gmail.com>
2016-03-10 15:55:30 +1100

    finish uers edit, update, index and destroy actions

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "unsuccessful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    patch user_path(@user), user: { name: "", email: "user@invalid.com", password: "foo", password_confirmation: "bar"}
+    assert_template 'users/edit'                                    
+  end
+
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name:  name,
+                                    email: email,
+                                    password:              "",
+                                    password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+end
